13a4f533de4d077f8666c024b280469abe1ba0c0
nikolajsmirnov <n.smirnov@autodoc.eu>
2023-10-26 19:59:34 +0300

    uod prod

--- src/main/java/org/example/springjava/MusicPlayer.java
+++ src/main/java/org/example/springjava/MusicPlayer.java
@@ -2,9 +2,39 @@ package org.example.springjava;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int value;
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public MusicPlayer(){
+
     }
 
     public void playMusic(){
